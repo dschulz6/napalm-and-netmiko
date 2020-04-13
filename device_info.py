a71b459da0629700ba6c1d04d3e8f7034c660c0d
Dale C. Schulz <daleschulz6@gmail.com>
2020-04-12 21:26:56 -0600

    Add files via upload

---
+++ device_info.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+"""
+used with my_netmiko_router6.py
+
+
+
+"""
+
+ios_xe1 = {
+             "address": "192.168.0.200",
+#             "netconf_port": 10000,
+#            "restconf_port": 9443,
+#             "ssh_port": 8181,
+             "username": "Qwest",
+             "password": "ilfnb980",
+             "device_type": "cisco_ios"
+          }
+
+# DevNet Always-On Sandbox NX-OS
+#
+#nxos1 = {
+#             "address": "sbx-nxos-mgmt.cisco.com",
+#             "netconf_port": 10000,
+#             "restconf_port": 443,
+#             "ssh_port": 818122,
+#             "username": "admin",
+#             "password": "Admin_1234!",
+#             "device_type": "cisco_nxos"
+#         }
+  
